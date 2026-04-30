1b15d2b9f98d0e48271137dfe2b9d93b809cc35a
Shambel Dechu <shambeldechu@gmail.com>
2026-04-30 15:08:31 +0300

    feat: make responsive for mobile

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -5,8 +5,11 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const onScroll = () => setIsScrolled(window.scrollY > 10);
@@ -15,47 +18,100 @@ const Header = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
+  useEffect(() => {
+    const onResize = () => {
+      if (window.innerWidth > 900) setIsMenuOpen(false);
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
+  const navItems = [
+    "Home",
+    "TV Shows",
+    "Movies",
+    "New & Popular",
+    "Latest",
+    "Browse by Languages",
+    "Help",
+  ];
+
   return (
-    <header
-      className={`header_outer_container ${isScrolled ? "header_scrolled" : ""}`}
-    >
-      <div className="header_inner_container">
-        <div className="header_left">
-          <ul aria-label="Primary navigation">
-            <li>
-              <img
-                src={netflixLogo}
-                alt="netflix logo"
-                className="netflix_logo"
-              />
-            </li>
-            <li>Home</li>
-            <li>TV Shows</li>
-            <li>Movies</li>
-            <li>New & Popular</li>
-            <li>Latest</li>
-            <li>Browse by Languages</li>
-            <li>Help</li>
-          </ul>
-        </div>
-        <div className="header_right">
-          <ul aria-label="Header actions">
-            <li>
+    <>
+      {isMenuOpen && (
+        <button
+          type="button"
+          className="header_backdrop"
+          aria-label="Close menu"
+          onClick={() => setIsMenuOpen(false)}
+        />
+      )}
+      <header
+        className={`header_outer_container ${isScrolled ? "header_scrolled" : ""}`}
+      >
+        <div className="header_inner_container">
+          <div className="header_left">
+            <img src={netflixLogo} alt="Netflix" className="netflix_logo" />
+
+            <nav
+              className={`header_nav ${isMenuOpen ? "header_nav_open" : ""}`}
+              aria-label="Primary navigation"
+            >
+              <ul className="header_nav_list">
+                {navItems.map((label) => (
+                  <li key={label} className="header_nav_item">
+                    <button
+                      type="button"
+                      className="header_nav_link"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      {label}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+
+          <div className="header_right" aria-label="Header actions">
+            <button type="button" className="header_icon_button" aria-label="Search">
               <SearchIcon />
-            </li>
-            <li>
+            </button>
+            <button
+              type="button"
+              className="header_icon_button header_desktop_only"
+              aria-label="Notifications"
+            >
               <NotificationsNoneIcon />
-            </li>
-            <li>
+            </button>
+            <button
+              type="button"
+              className="header_icon_button header_desktop_only"
+              aria-label="Account"
+            >
               <AccountBoxIcon />
-            </li>
-            <li>
+            </button>
+            <button
+              type="button"
+              className="header_icon_button header_desktop_only"
+              aria-label="More"
+            >
               <ArrowDropDownIcon />
-            </li>
-          </ul>
+            </button>
+
+            <button
+              type="button"
+              className="header_icon_button header_mobile_only"
+              aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+              aria-expanded={isMenuOpen}
+              onClick={() => setIsMenuOpen((open) => !open)}
+            >
+              {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
