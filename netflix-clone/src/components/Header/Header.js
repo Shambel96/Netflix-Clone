4377656543a9e66381229c4f94564f07acdc2270
Shambel Dechu <shambeldechu@gmail.com>
2026-04-30 14:47:58 +0300

    feat: update the Header

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import netflixLogo from "../../assets/images/netflix-logo.jpg";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,11 +6,22 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => setIsScrolled(window.scrollY > 10);
+    onScroll();
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div className="header_outer_container">
+    <header
+      className={`header_outer_container ${isScrolled ? "header_scrolled" : ""}`}
+    >
       <div className="header_inner_container">
         <div className="header_left">
-          <ul>
+          <ul aria-label="Primary navigation">
             <li>
               <img
                 src={netflixLogo}
@@ -28,16 +39,23 @@ const Header = () => {
           </ul>
         </div>
         <div className="header_right">
-          <ul>
-            <li><SearchIcon /></li>
-            <li><NotificationsNoneIcon /></li>
-            <li><AccountBoxIcon /></li>
-            <li><ArrowDropDownIcon /></li>
+          <ul aria-label="Header actions">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
           </ul>
-          
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
